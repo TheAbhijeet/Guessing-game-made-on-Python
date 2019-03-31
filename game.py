eb88fc52e26638b6ca07b0ef68350965fa66b478
TheAbhijeet <38559396+TheAbhijeet@users.noreply.github.com>
2019-03-31 14:24:31 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random
 number = random.randint(1, 10)
-print("Hello, What's your name?")
-player_name = input()
+
+player_name = input("Hello, What's your name?")
 number_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
+
 while number_of_guesses < 5:
-    guess = input()
-    guess = int(guess)
-    number_of_guesses = number_of_guesses + 1
+    guess = int(input())
+    number_of_guesses += 1
     if guess < number:
         print('Your guess is too low')
     if guess > number:
